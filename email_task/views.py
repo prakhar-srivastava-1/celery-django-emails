edddbe06855600467f4c0321155d62f678668e91
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-06-25 17:30:54 +0530

    add ReviewFormView in views.py and send_email method in forms.py

--- email_task/views.py
+++ email_task/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
+
+from .forms import ReviewForm
 
 # Create your views here.
+class ReviewFormView(FormView):
+    form_class = 'ReviewForm'
+    template_name = 'review.html'
+
+    def form_valid(self, form):
+        form.send_email()
+        msg = "Thanks for the review!"
+        return HttpResponse(msg)
