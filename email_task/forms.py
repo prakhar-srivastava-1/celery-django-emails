0119f20cd1be91d5cb64f3c2f603184e6f0bc161
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-06-25 17:22:28 +0530

    add new email_task app and write forms.py

---
+++ email_task/forms.py
@@ -0,0 +1,38 @@
+from socket import fromshare
+from django import forms
+
+class RevierForm(forms.Form):
+    name = forms.CharField(
+        label='Name',
+        mine_length=4,
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control-mb-3',
+                'placeholder': 'Name',
+                'id': 'name-field'
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control-mb-3',
+                'placeholder': 'Email',
+                'id': 'email-field'
+            }
+        )
+    )
+    review = forms.CharField(
+        label='Review',
+        mine_length=4,
+        max_length=200,
+        widget=forms.TextArea(
+            attrs={
+                'class': 'form-control',
+                'rows': '5',
+            }
+        )
+    )
