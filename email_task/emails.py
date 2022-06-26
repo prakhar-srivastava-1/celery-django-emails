2c9d8c8c1b5db165376d0ee75c2ee5933caa27bc
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-06-26 10:21:53 +0530

    finalise and test email

--- email_task/emails.py
+++ email_task/emails.py
@@ -2,13 +2,15 @@
 # We will be able to use the keys to get the values there
 from email.message import EmailMessage
 from django.template import Context
+from django.core.mail import EmailMessage
 
 # render_to_string will render the data fetched from email_message.txt file
 # as a string with the required variables evaluated and embedded in it
 from django.template.loader import render_to_string
 
 # import secrets
-from .secrets import EMAIL_ID, PASSWORD
+# from .secrets import EMAIL_ID, PASSWORD
+from core import settings
 
 def send_review_email(name, email, review):
     # This dictionary will be passed to email_message.txt
@@ -29,7 +31,8 @@ def send_review_email(name, email, review):
     email_body = render_to_string("email_message.txt", context)
 
     email = EmailMessage(
-        email_subject, email_body,
+        email_subject, 
+        email_body,
         #source email
         settings.DEFAULT_FROM_EMAIL, 
         #target email
